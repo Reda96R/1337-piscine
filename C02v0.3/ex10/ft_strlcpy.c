5f648f23777e948ff711e6ecc57ab2c56e1143b4
Reda96R <redarayyad@yahoo.com>
2022-06-08 21:13:52 +0100

    adding strlcpy

---
+++ C02v0.3/ex10/ft_strlcpy.c
@@ -0,0 +1,20 @@
+unsigned int	ft_strlcpy(char	*dest, char	*src, unsigned int	size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while(src[j])
+		j++;
+	if (size != 0)
+	{
+		while(src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0'
+	}
+	return(j);
+}
